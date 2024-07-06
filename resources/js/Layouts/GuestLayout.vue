ed9f1b4e49c35675e5aa22bf92463cacf60224de
Albert Cloete <cloete.albert@gmail.com>
2024-07-06 20:27:31 +0200

    Updated theme colors.

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -22,8 +22,8 @@ const mode = useColorMode()
         <header class="absolute inset-x-0 top-0 z-50">
             <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <Link href="/" class="flex lg:flex-1 text-2xl font-bold text-gradient-to-tr from-yellow-400 to-red-600">
-                    <h1 class="bg-gradient-to-b from-orange-900 to-[#FF9C65] dark:from-white dark:to-[#FF9C65] bg-clip-text sm:text-xl text-2xl lg:text-3xl font-semibold text-transparent">
-                        Job <br/>Refinery
+                    <h1 class="text-primary sm:text-xl text-2xl lg:text-3xl font-semibold">
+                        Job<span class="text-foreground">Refinery</span>
                     </h1>
                 </Link>
                 <div class="flex lg:hidden">
@@ -36,7 +36,7 @@ const mode = useColorMode()
                 </div>
                 <div class="hidden lg:flex lg:gap-x-12">
                     <a v-for="item in navigation" :key="item.name" :href="item.href"
-                       class="text-sm font-semibold leading-6 text-stone-700 dark:text-stone-200">{{ item.name }}</a>
+                       class="text-sm font-semibold leading-6 text-popover-foreground">{{ item.name }}</a>
                 </div>
                 <div class="hidden lg:flex lg:flex-1 lg:justify-end">
                     <Button variant="outline" class="mr-8" @click="mode = mode === 'dark' ? 'light' : 'dark'">
@@ -47,7 +47,7 @@ const mode = useColorMode()
                         <span class="sr-only">Toggle theme</span>
                     </Button>
                     <a href="#"
-                       class="flex items-center justify-center text-sm font-semibold text-stone-700 dark:text-stone-200">
+                       class="flex items-center justify-center text-sm font-semibold text-popover-foreground">
                         Sign in <span aria-hidden="true">&rarr;</span>
                     </a>
                 </div>
@@ -89,23 +89,11 @@ const mode = useColorMode()
                 </DialogPanel>
             </Dialog>
         </header>
-        <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-             aria-hidden="true">
-            <div
-                class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-yellow-400 to-red-600 opacity-70 dark:opacity-70 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-                style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"/>
-        </div>
+
         <div class="flex items-center justify-center min-h-screen">
 
         <slot></slot>
 
-        </div>
-        <div
-            class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-            aria-hidden="true">
-            <div
-                class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-yellow-400 to-red-600 opacity-70 dark:opacity-70 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-                style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"/>
         </div>
     </div>
 </template>
