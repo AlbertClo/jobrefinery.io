44b906d269a876426e60b463cb6ffaed58056967
Albert Cloete <cloete.albert@gmail.com>
2024-11-10 23:06:29 +0200

    Add start-dev.sh script and zellij layout.

---
+++ start-dev.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# kill all screen sessions
+pkill screen
+
+# stop containers, if any are running
+running_containers=$(docker ps -q) && [ -n "$running_containers" ] && docker kill $running_containers || echo "No running containers found."
+
+./vendor/bin/sail up -d
+
+screen -dmS vite
+screen -dmS storybook
+screen -dmS queue-default
+screen -dmS queue-llm
+screen -dmS pail
+screen -dmS dump-server
+
+screen -S vite -X stuff './vendor/bin/sail npm run dev\n'
+screen -S storybook -X stuff './vendor/bin/sail npm run storybook\n'
+screen -S queue-default -X stuff './vendor/bin/sail artisan queue:listen --tries=1\n'
+screen -S queue-llm -X stuff './vendor/bin/sail artisan queue:listen --queue=prompt-llm\n'
+screen -S pail -X stuff './vendor/bin/sail artisan pail\n'
+screen -S dump-server -X stuff './vendor/bin/sail artisan dump-server\n'
