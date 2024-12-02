f1d1bf8335159f3dfd744ce6caa995e9930a1e15
Albert Clo <cloete.albert@gmail.com>
2024-12-02 03:08:59 +0200

    Change screen name for debug screen in start-dev.sh.

--- start-dev.sh
+++ start-dev.sh
@@ -13,11 +13,11 @@ screen -dmS storybook
 screen -dmS queue-default
 screen -dmS queue-llm
 screen -dmS pail
-screen -dmS dump-server
+screen -dmS debug
 
 screen -S vite -X stuff './vendor/bin/sail npm run dev\n'
 screen -S storybook -X stuff './vendor/bin/sail npm run storybook\n'
 screen -S queue-default -X stuff './vendor/bin/sail artisan queue:listen --tries=1\n'
 screen -S queue-llm -X stuff './vendor/bin/sail artisan queue:listen --queue=prompt-llm\n'
 screen -S pail -X stuff './vendor/bin/sail artisan pail\n'
-screen -S dump-server -X stuff './vendor/bin/sail artisan dump-server\n'
+screen -S debug -X stuff './vendor/bin/sail artisan dump-server\n'
